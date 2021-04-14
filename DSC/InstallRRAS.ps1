6a7e95ffc500a0dec4495c0432563543f0fe5ce6
www42 <thomas.jaekel@gfn.de>
2021-04-14 22:00:27 +0200

    enhanced ie sec

--- DSC/InstallRRAS.ps1
+++ DSC/InstallRRAS.ps1
@@ -22,6 +22,7 @@ configuration InstallRRAS
             Ensure = 'Present'
             Key = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
             ValueName = 'IsInstalled'
+            ValueType = 'Dword'
             ValueData = [int]0
         }
     }
